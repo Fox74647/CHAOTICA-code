d2539695ac2aca6d9ae166ce5d787fefd6a88ad5
Fox Chaotica <foxchaotica@gmail.com>
2017-05-17 14:13:37 +0000

    Added Coin.cs
    A coin script to go with the money system

---
+++ Coin.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour {
+
+	private CoinManager Money;
+	private GameObject Player;
+	public int CoinValue;
+
+	// Use this for initialization
+	void Start () {
+		Money = GameObject.Find("CoinManager").GetComponent<CoinManager>();	
+		Player = GameObject.Find("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		gameObject.transform.Rotate(0, 1, 0);
+
+		if (Vector3.Distance(Player.transform.position, gameObject.transform.position) <= 2)
+		{
+			Money.MoneyDifference += CoinValue;
+			Destroy(gameObject);
+		}
+	}
+
+}
