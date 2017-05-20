5dc721ce308bcba29ae5a96543215835462095ba
Fox Chaotica <foxchaotica@gmail.com>
2017-05-20 14:51:46 +0000

    Added Level System
    the base information for an experience system

---
+++ XPLevels.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class XPLevels : MonoBehaviour {
+
+	private int Lvls = 64, MinLvlXP = 1024, MaxLvlXP = 1048576;
+	public int[] Levels;
+
+	// Use this for initialization
+	void Start () {
+		Levels = new int[Lvls];
+		double B = Mathf.Log((float)MaxLvlXP / MinLvlXP) / (Lvls - 1);
+		double A = (float)MinLvlXP / (Mathf.Exp((float)B) - 1.0);
+		for (int i = 1; i <= Lvls; i++)
+		{
+			int old_xp = (int)Mathf.Round((float)A * Mathf.Exp((float)B * (i - 1)));
+			int new_xp = (int)Mathf.Round((float)A * Mathf.Exp((float)B * i));
+			Levels[i-1] = (new_xp - old_xp);
+		}
+		Levels[63] = MaxLvlXP;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
