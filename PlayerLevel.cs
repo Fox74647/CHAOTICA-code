3547dd0bd334041043bc64a9473045e7fd1b40ea
Fox Chaotica <foxchaotica@gmail.com>
2017-05-20 17:59:23 +0000

    Added Player XP counter
    a script that holds the total experience the player has, as well as the current level they're at

---
+++ PlayerLevel.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLevel : MonoBehaviour {
+
+	public int CurrentLevel, TotalExperience, MaxExperience;
+	XPLevels LevelSystem;
+
+
+	// Use this for initialization
+	void Start () {
+		LevelSystem = GameObject.Find("LevelManager").GetComponent<XPLevels>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		int temp = LevelSystem.Levels[0];
+		if (MaxExperience == 0)
+		{
+			for (int i = 0; i < 64; i++)
+			{
+				MaxExperience += LevelSystem.Levels[i];
+			}
+		}
+		if (TotalExperience >= MaxExperience)
+		{
+			CurrentLevel = 64;
+		}
+		else
+		{
+			for (int i = 0; i < 64; i++)
+			{
+				if (TotalExperience >= temp)
+				{
+					temp += LevelSystem.Levels[i+1];
+				}
+				else
+				{
+					CurrentLevel = i;
+					break;
+				}
+			}
+		}
+	}
+}
