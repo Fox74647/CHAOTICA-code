0011131ff92eab821e699508ee6a33f8dbb22577
Fox Chaotica <foxchaotica@gmail.com>
2017-05-24 14:29:01 +0000

    Updated Experience Display
    Now also shows current level

--- ExperienceDisplay.cs
+++ ExperienceDisplay.cs
@@ -4,20 +4,27 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class ExperienceDisplay : MonoBehaviour {
+	/*
+	 * This script displays the current experience level,
+	 * total experience needed to gain a level,
+	 * and the amount of experience within the current level.
+	 */
 
 	XPLevels LevelSystem;
 	PlayerLevel PlayerXP;
-	Text XPCounter;
+	Text XPCounter, LevelCounter;
 
 	// Use this for initialization
 	void Start () {
 		LevelSystem = GameObject.Find("LevelManager").GetComponent<XPLevels>();
 		PlayerXP = GameObject.Find("Player").GetComponent<PlayerLevel>();
-		XPCounter = GetComponent<Text> () as Text;
+		XPCounter = GameObject.Find("XPCounter").GetComponent<Text> () as Text;
+		LevelCounter = GameObject.Find("LevelCounter").GetComponent<Text> () as Text;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		LevelCounter.text = "Level " + PlayerXP.CurrentLevel.ToString();
 		if(PlayerXP.CurrentLevel == 64)
 		{
 			XPCounter.text = LevelSystem.Levels[63].ToString() + "/" + LevelSystem.Levels[63].ToString();
