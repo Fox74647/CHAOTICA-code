2f3dc5d210e7284070e00588ceebe14d6773d0ec
Fox Chaotica <foxchaotica@gmail.com>
2017-05-24 14:03:58 +0000

    Added Experience Display
    a display that shows how many more XP points are required to gain a level

---
+++ ExperienceDisplay.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ExperienceDisplay : MonoBehaviour {
+
+	XPLevels LevelSystem;
+	PlayerLevel PlayerXP;
+	Text XPCounter;
+
+	// Use this for initialization
+	void Start () {
+		LevelSystem = GameObject.Find("LevelManager").GetComponent<XPLevels>();
+		PlayerXP = GameObject.Find("Player").GetComponent<PlayerLevel>();
+		XPCounter = GetComponent<Text> () as Text;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(PlayerXP.CurrentLevel == 64)
+		{
+			XPCounter.text = LevelSystem.Levels[63].ToString() + "/" + LevelSystem.Levels[63].ToString();
+		}
+		else
+		{
+			int temp = 0;
+			for (int i = 0; i<PlayerXP.CurrentLevel; i++)
+			{
+				temp +=LevelSystem.Levels[i];
+			}
+			XPCounter.text = (PlayerXP.TotalExperience-temp).ToString() + "/" + LevelSystem.Levels[PlayerXP.CurrentLevel].ToString();
+		}
+			
+	}
+}
