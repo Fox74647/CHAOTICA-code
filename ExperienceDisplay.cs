d8390c6568306186bb7b52c011989eaaeb01b5af
Fox Chaotica <foxchaotica@gmail.com>
2017-05-25 06:11:35 +0000

    Added XP Bar

--- ExperienceDisplay.cs
+++ ExperienceDisplay.cs
@@ -13,6 +13,7 @@ public class ExperienceDisplay : MonoBehaviour {
 	XPLevels LevelSystem;
 	PlayerLevel PlayerXP;
 	Text XPCounter, LevelCounter;
+	Slider XPBar;
 
 	// Use this for initialization
 	void Start () {
@@ -20,6 +21,7 @@ public class ExperienceDisplay : MonoBehaviour {
 		PlayerXP = GameObject.Find("Player").GetComponent<PlayerLevel>();
 		XPCounter = GameObject.Find("XPCounter").GetComponent<Text> () as Text;
 		LevelCounter = GameObject.Find("LevelCounter").GetComponent<Text> () as Text;
+		XPBar = GameObject.Find("XPBar").GetComponent<Slider> () as Slider;
 	}
 	
 	// Update is called once per frame
@@ -28,6 +30,8 @@ public class ExperienceDisplay : MonoBehaviour {
 		if(PlayerXP.CurrentLevel == 64)
 		{
 			XPCounter.text = LevelSystem.Levels[63].ToString() + "/" + LevelSystem.Levels[63].ToString();
+			XPBar.maxValue = LevelSystem.Levels[63];
+			XPBar.value = LevelSystem.Levels[63];
 		}
 		else
 		{
@@ -37,6 +41,8 @@ public class ExperienceDisplay : MonoBehaviour {
 				temp +=LevelSystem.Levels[i];
 			}
 			XPCounter.text = (PlayerXP.TotalExperience-temp).ToString() + "/" + LevelSystem.Levels[PlayerXP.CurrentLevel].ToString();
+			XPBar.maxValue = LevelSystem.Levels[PlayerXP.CurrentLevel];
+			XPBar.value = PlayerXP.TotalExperience-temp;
 		}
 			
 	}
