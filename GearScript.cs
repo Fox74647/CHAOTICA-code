e87a610f414a199315235f60e68e715cf00d5cc3
Fox Chaotica <foxchaotica@gmail.com>
2017-10-27 12:26:53 +0000

    Added GearScript
    Added a script which accurately animates gears, with correct ratios.

---
+++ GearScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GearScript : MonoBehaviour {
+
+	public int teeth;
+	public float speed;
+	public GameObject PrevGear;
+	public bool ConnectedByShaft;
+	private float angle = 0f;
+	public float ratio = 0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (PrevGear != null)
+		{
+			speed = PrevGear.transform.GetComponent<GearScript>().speed;
+			if (!ConnectedByShaft)
+			{
+				ratio = -1.0f * ((float)PrevGear.transform.GetComponent<GearScript>().teeth/(float)teeth);
+				speed *= ratio;
+			}
+		}
+		angle += speed * Time.deltaTime;
+		gameObject.transform.eulerAngles = new Vector3 (transform.rotation.x, angle, transform.rotation.z);
+	}
+}
